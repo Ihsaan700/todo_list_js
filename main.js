6db47f8af8f5db4306e8ad89c4b35e6c9db4b99f
Ihsaanul Hoque <ihsaanul.hoque@hotmail.com>
2020-08-17 00:14:40 +0100

    escaped the console via refactoring

--- main.js
+++ main.js
@@ -1,38 +1,21 @@
 todoList = {
   todos: [],
-  displayTodos: function() {
-    if (this.todos.length === 0) {
-      console.log('you have no todos!')
-    } else {
-      console.log('My todos: ')
-      for (i = 0; i < this.todos.length; i++) {
-        if (this.todos[i].completed === true) {
-          console.log('(x)',this.todos[i].todoText) 
-        } else {
-          console.log('( )',this.todos[i].todoText)
-        }
-      }
-    }
-  },
+
   addTodo: function(todoText) {
     this.todos.push({
       todoText: todoText,
       completed: false
     })
-    this.displayTodos()
   },
   changeTodo: function(index, newText) {  
     this.todos[index].todoText = newText
-    this.displayTodos()
   },
   deleteTodo: function(index, range = 1) {
     this.todos.splice(index, range)
-    this.displayTodos()
   },
   toggleCompleted: function(index) {
     todo = this.todos[index]
     todo.completed = !todo.completed
-    this.displayTodos()
   },
   toggleAll: function() {
     totalTodos = this.todos.length
@@ -46,24 +29,20 @@ todoList = {
       for (i = 0; i < totalTodos; i++) {
         this.todos[i].completed = false
       }
-      this.displayTodos()
     } else {
       for (i = 0; i < totalTodos; i++) {
         this.todos[i].completed = true
       }
-      this.displayTodos()
     }
   }
 }
 
 handlers = {
-  displayTodos: function() {
-    todoList.displayTodos()    
-  },
   addTodo: function() {
     addTodoTextInput = document.getElementById('addTodoTextInput')
     todoList.addTodo(addTodoTextInput.value)
     addTodoTextInput.value = ''
+    view.displayTodos()
   },
   changeTodo: function() {
     changeTodoPositionInput = document.getElementById('ChangeTodoPosition')
@@ -71,19 +50,23 @@ handlers = {
     todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoInputText.value)
     changeTodoPositionInput.value = ''
     changeTodoInputText.value = ''
+    view.displayTodos()
   },
   deleteTodo: function() {
     deleteTodoPositionInput = document.getElementById('deletePositionInput')
     todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber)
     deleteTodoPositionInput.value = ''
+    view.displayTodos()
   },
   toggleTodo: function() {
     toggledTodo = document.getElementById('toggleTodoInput')
     todoList.toggleCompleted(toggledTodo.valueAsNumber)
     toggleTodo = ''
+    view.displayTodos()
   },
   toggleAllTodos: function() {
     todoList.toggleAll()
+    view.displayTodos()
   }
 }
 
